6dfd79dab68b2456a4b01d468e4507fec0bf77f8
adfar-136 <adfarrasheed136@gmail.com>
2024-10-26 22:51:44 +0530

    25-10-2024

---
+++ js/datatypes.js
@@ -0,0 +1,24 @@
+// var a = 10;
+// console.log(typeof a);
+// var b = '12345678';
+// var c = 12345678;
+// console.log(b===c)
+
+// console.log(typeof b)
+
+// var a =true;
+// var b = 1;
+// var c ="1"
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a === c)
+// console.log(a==c);
+// var a;
+// console.log(typeof a)
+
+// var b = null;
+// console.log(null == 0)
+
+// var a = 2344554435344242343431234n;
+// console.log(typeof a);
+console.log("24"/4)
---
+++ js/index.js
@@ -0,0 +1,33 @@
+// var a= 10;
+// console.log(a)
+// a = "adfarrrrr";
+// console.log(a)
+// console.log(a)
+// var a="adfa";
+// console.log(a)
+//  let b = 2;
+//  console.log(b)
+//  b= 22222;
+// //  let b =45;
+//  console.log(b)
+
+// const c =30;
+// console.log(c)
+// // const c =45;
+// c= 34;
+// console.log(c)
+
+
+var a ;
+a=35;
+console.log(a)
+let b;
+b = 45;
+console.log(b)
+
+// const c;
+// c =67;
+// console.log(c)
+
+
+
