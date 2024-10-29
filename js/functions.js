ccd8a2f7c81c3f34d3224ef55d768ccd6cf20874
adfar-136 <adfarrasheed136@gmail.com>
2024-10-29 22:51:37 +0530

    29-10-2024

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,42 @@
-// function greet(x){
-//     console.log(x)
-//     return "adfar","t"
+// // // function greet(x){
+// // //     console.log(x)
+// // //     return "adfar","t"
+// // // }
+// // // var a = greet(4,5)
+// // // console.log(a)
+// // // greet(66,6)
+
+// // //f(x,y) = 4x+3y
+// // //f(2) =11
+// // //f(3)=15
+// // //f(33)=135
+
+// // //1. function statement
+// // //2.function experessons;
+// // //3.anonymous function;
+// // //4.fat arrow function;
+// // //5. callback function;
+// // //6.higer order functions;
+
+// // var greet =function (x,y,z){
+// //     return x+y+z
+// // }
+// // console.log(greet(4,5,6))
+
+// function greet(){
+//     var a = 10;
+//     function add(){
+//         console.log("adfar")
+//     }
+//     add()
 // }
-// var a = greet(4,5)
-// console.log(a)
-// greet(66,6)
 
-//f(x,y) = 4x+3y
-//f(2) =11
-//f(3)=15
-//f(33)=135
+// gree
 
-//1. function statement
-//2.function experessons;
-//3.anonymous function;
-//4.fat arrow function;
-//5. callback function;
-//6.higer order functions;
-
-var greet =function (x,y,z){
-    return x+y+z
+console.log(add)
+function greet(){
+    console.log("Good morning")
 }
-console.log(greet(4,5,6))
+var add = function(){
+    console.log("Hello morrow")
+}
---
+++ js/functiontypes.js
@@ -0,0 +1,37 @@
+// var greet=function(x,y){
+//    console.log(x+y)
+// }
+// var greet=(x,y)=>{
+//     console.log("Adfar")
+//     console.log("Rasheed")
+//     console.log(x+y)
+// }
+// var greet = function(x){
+//     return x+x
+// }
+// var greet = x=>x+x
+// var t = greet(5)
+// console.log(t)
+// function greet(){
+//     console.log("Good morning")
+// }
+// function add(x,y){
+//     x()
+//     console.log(y)
+// }
+// add(greet,"hg")
+// function greet(){
+//     return function(){
+
+//     }
+// }
+// function add(x,y){
+//     x()
+//     console.log(y)
+// }
+// add(()=>{
+//     console.log("ddffghj")
+// },7)
+(function(){
+    console.log("Adfar")
+}())
